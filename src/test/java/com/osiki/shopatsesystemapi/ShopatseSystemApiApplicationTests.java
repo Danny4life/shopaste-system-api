4cd6eb27f518f1e880dd66177bf145769a6ee746
Segun Osiki <osiki.segun@gmail.com>
2023-01-13 07:19:50 +0100

    add initail file

---
+++ src/test/java/com/osiki/shopatsesystemapi/ShopatseSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiki.shopatsesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopatseSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
