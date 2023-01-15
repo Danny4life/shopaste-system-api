88677de8885b3bacd258c82408a90de9036ed472
Segun Osiki <osiki.segun@gmail.com>
2023-01-15 09:41:37 +0100

    add fields to Employee entity class

--- src/main/java/com/osiki/shopatsesystemapi/entity/EmployeeEntity.java
+++ src/main/java/com/osiki/shopatsesystemapi/entity/EmployeeEntity.java
@@ -2,11 +2,17 @@ package com.osiki.shopatsesystemapi.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Table(name = "employees")
 public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
