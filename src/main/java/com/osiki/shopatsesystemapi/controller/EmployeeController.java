b5cff55dbdf025ea8f82b9159f14cda6e95379ad
Segun Osiki <osiki.segun@gmail.com>
2023-01-15 09:33:16 +0100

    add employee service dependency to employee controller

--- src/main/java/com/osiki/shopatsesystemapi/controller/EmployeeController.java
+++ src/main/java/com/osiki/shopatsesystemapi/controller/EmployeeController.java
@@ -1,10 +1,17 @@
 package com.osiki.shopatsesystemapi.controller;
 
 
+import com.osiki.shopatsesystemapi.services.EmployeeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 }
