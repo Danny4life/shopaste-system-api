af59ca36720fbe740382eb719a3d6a2f0b5e90cf
Segun Osiki <osiki.segun@gmail.com>
2023-01-15 09:46:17 +0100

    created Employee repository class

---
+++ src/main/java/com/osiki/shopatsesystemapi/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.osiki.shopatsesystemapi.repository;
+
+import com.osiki.shopatsesystemapi.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
