208a50b32322a3bc83809390435a89e7e2481f10
Segun Osiki <osiki.segun@gmail.com>
2023-01-15 09:37:59 +0100

    created Employee entity class

---
+++ src/main/java/com/osiki/shopatsesystemapi/entity/EmployeeEntity.java
@@ -0,0 +1,12 @@
+package com.osiki.shopatsesystemapi.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "employees")
+public class EmployeeEntity {
+}
