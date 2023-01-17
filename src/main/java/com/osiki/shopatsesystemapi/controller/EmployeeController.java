f12b294787235ef12f1aeb7cb35692a7bb6f5a86
Segun Osiki <osiki.segun@gmail.com>
2023-01-17 07:59:25 +0100

    add cross origin annotation to controller class

--- src/main/java/com/osiki/shopatsesystemapi/controller/EmployeeController.java
+++ src/main/java/com/osiki/shopatsesystemapi/controller/EmployeeController.java
@@ -3,11 +3,9 @@ package com.osiki.shopatsesystemapi.controller;
 
 import com.osiki.shopatsesystemapi.model.EmployeeModel;
 import com.osiki.shopatsesystemapi.services.EmployeeService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
