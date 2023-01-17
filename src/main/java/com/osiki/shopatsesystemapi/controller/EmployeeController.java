3a0c8498bac8b3e557c405074b6e781b9513c855
Segun Osiki <osiki.segun@gmail.com>
2023-01-17 06:46:32 +0100

    add create employee api in controller class

--- src/main/java/com/osiki/shopatsesystemapi/controller/EmployeeController.java
+++ src/main/java/com/osiki/shopatsesystemapi/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.osiki.shopatsesystemapi.controller;
 
 
+import com.osiki.shopatsesystemapi.model.EmployeeModel;
 import com.osiki.shopatsesystemapi.services.EmployeeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,10 @@ public class EmployeeController {
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @PostMapping("/employees")
+    public EmployeeModel createEmployee(@RequestBody EmployeeModel employeeModel){
+        return employeeService.createEmployee(employeeModel);
     }
 }
--- src/main/java/com/osiki/shopatsesystemapi/services/EmployeeService.java
+++ src/main/java/com/osiki/shopatsesystemapi/services/EmployeeService.java
@@ -1,4 +1,7 @@
 package com.osiki.shopatsesystemapi.services;
 
+import com.osiki.shopatsesystemapi.model.EmployeeModel;
+
 public interface EmployeeService {
+    EmployeeModel createEmployee(EmployeeModel employeeModel);
 }
