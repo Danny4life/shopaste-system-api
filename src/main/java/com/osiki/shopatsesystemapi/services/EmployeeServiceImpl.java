09ecc8c84921f5b44fd8b24d21a47c1fca8fa2aa
Segun Osiki <osiki.segun@gmail.com>
2023-01-17 06:53:03 +0100

    add create employee implementation in service class

--- src/main/java/com/osiki/shopatsesystemapi/services/EmployeeServiceImpl.java
+++ src/main/java/com/osiki/shopatsesystemapi/services/EmployeeServiceImpl.java
@@ -1,7 +1,27 @@
 package com.osiki.shopatsesystemapi.services;
 
+import com.osiki.shopatsesystemapi.entity.EmployeeEntity;
+import com.osiki.shopatsesystemapi.model.EmployeeModel;
+import com.osiki.shopatsesystemapi.repository.EmployeeRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public EmployeeModel createEmployee(EmployeeModel employeeModel) {
+
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+
+        BeanUtils.copyProperties(employeeModel, employeeEntity);
+        employeeRepository.save(employeeEntity);
+        return employeeModel;
+    }
 }
